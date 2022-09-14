cfcc704986414894a99f8aff537cf57ed9794e1b
Michaelag32 <Mikelagg32@gmail.com>
2022-09-14 14:08:19 -0700

    practicing medium challenges

--- app.js
+++ app.js
@@ -77,3 +77,68 @@ console.log (filterOutFalsy(['', [], 0, null, undefined, '0', 500])) */
 
 console.log (convertToBoolean ([500, 0, 'David', '', []])); */
 
+/* function filterOutFalsy (elem1, elem2) {
+if (!elem1 === true) {
+    return elem1
+}
+else {
+    return elem2
+}
+}
+// or return !elem ? elem1 : elem2
+console.log (filterOutFalsy(true, 'dog')) */
+
+
+/* function arrSum (arr) {
+    let sum = 0
+for (let i = 0; i < arr.length; ++i) {
+ sum = sum + arr[i]
+}
+return sum
+}
+
+console.log(arrSum([2, 2, 2])) */
+
+
+/* function ProgressiveSum(num) {
+    let sum = 0 
+    for (let i = 0; i <= num; ++i) {
+        sum = sum + i;
+    }
+    return sum
+}
+console.log(ProgressiveSum(3)); */
+
+/* function getMax (arr) {
+    let max = arr[0]
+    for (let i = 0; i < arr.length; ++i) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max
+}
+console.log (getMax ([5, 100, 0,])) */
+
+
+/* function calcTime (seconds) {
+    let timerMinutes = Math.floor (seconds / 60)
+    let timerSeconds = seconds % 60
+
+    if (timerMinutes.toString().length === 1) {
+        timerMinutes = '0' + timerMinutes
+    }
+
+    return timerMinutes + ":" + timerSeconds
+}
+console.log (calcTime(500)) */
+
+/*function convertToZeros (arr) {
+    for (let i = 0; i < arr.length; ++i){
+        arr[i] = 0
+    }
+    return arr
+}
+
+console.log (convertToZeros([5, 100, 22])) */
+
